7d9b61d9d0e4cea00bfa4aad597a5ec2becc8849
NareshKoirala <koiralanaresh10@gmail.com>
2026-03-12 22:22:13 -0600

    Working with scrapping the fotmob website

--- producer/fotmob_scrapper.py
+++ producer/fotmob_scrapper.py
@@ -2,11 +2,16 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 from pprint import pprint
 
-base_url = "https://www.fotmob.com/leagues/47/overview/premier-league"
+base_url = "https://www.fotmob.com/leagues/47/table/premier-league"
 request_raw_data = req.get(base_url)
 
 soup = bs(request_raw_data.text, features="html.parser")
-nav = soup.nav
 
-for a in nav.find_all('a'):
-    print(a['href'])
+#section_clipped = soup.article
+#print(section_clipped)
+
+for i in soup.find('div', class_='flipmove'):
+    print(i)
+
+def table_scrap(soup):
+    pass
