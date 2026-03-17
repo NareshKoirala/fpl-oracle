63432dcf7e1ec21b4b21ba1d756abe20bf6b3ae6
NareshKoirala <koiralanaresh10@gmail.com>
2026-03-16 23:38:48 -0600

    nav href scraping complete -> Now working on the table tab to get the teams position and there links with any other important detail about the team.

--- producer/fotmob_scrapper.py
+++ producer/fotmob_scrapper.py
@@ -1,17 +1,33 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
-from pprint import pprint
 
-base_url = "https://www.fotmob.com/leagues/47/table/premier-league"
-request_raw_data = req.get(base_url)
 
-soup = bs(request_raw_data.text, features="html.parser")
+class fotmob_scrapper:
+
+    def __init__(self):
+        self.base_url = "https://www.fotmob.com/leagues/47/table/premier-league"
+        request_raw_data = req.get(self.base_url)
+        self.soup = bs(request_raw_data.text, features="html.parser")
+    
+    def get_imp_links(self):
+        get_nav = self.soup.nav
+
+        for x in get_nav:
+            print(x.get('href'))
+    
+
+
+
+fotmob = fotmob_scrapper()
+fotmob.get_imp_links()
+
+
 
 #section_clipped = soup.article
 #print(section_clipped)
 
-for i in soup.find('div', class_='flipmove'):
-    print(i)
+#for i in soup.find('div', class_='flipmove'):
+#    print(i)
 
-def table_scrap(soup):
-    pass
+#def table_scrap(soup):
+#    pass
