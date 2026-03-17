bc2eded7c61565f4b22f57355219f35dc621f572
NareshKoirala <koiralanaresh10@gmail.com>
2026-03-17 01:30:01 -0600

    Came in a standoff of not being able to fetch fully loaded html, so transition fotmod_scrapper from requests to playwright

--- producer/fotmob_scrapper.py
+++ producer/fotmob_scrapper.py
@@ -1,33 +1,46 @@
+import time
+import random
 import requests as req
 from bs4 import BeautifulSoup as bs
-
+from playwright.sync_api import sync_playwright
 
 class fotmob_scrapper:
 
     def __init__(self):
         self.base_url = "https://www.fotmob.com/leagues/47/table/premier-league"
-        request_raw_data = req.get(self.base_url)
-        self.soup = bs(request_raw_data.text, features="html.parser")
+        self.soup = self.playwright_content(self.base_url)
+
+    def playwright_content(self, url):
+        with sync_playwright() as p:
+                # Launch headless (no window pops up)
+            browser = p.chromium.launch(headless=True)
+            context = browser.new_context(
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+            )
+            page = context.new_page()
+    
+            try:
+                page.goto(url)
+                
+                # This is the magic part: wait for the JS to 'paint' the table
+                # Replace '.stats-table' with the actual class name you saw in DevTools
+                page.wait_for_selector(".flipmove", timeout=10000) 
+                
+                # Now grab the 'cooked' HTML
+                content = page.content()
+                
+                return bs(content, 'html.parser')
+    
+            except Exception as e:
+                print(f"Error: {e}")
+            finally:
+                browser.close()
+
     
     def get_imp_links(self):
-        get_nav = self.soup.nav
+        get_ = self.soup.article
 
-        for x in get_nav:
-            print(x.get('href'))
-    
-
-
+        print(get_)
 
 fotmob = fotmob_scrapper()
 fotmob.get_imp_links()
-
-
-
-#section_clipped = soup.article
-#print(section_clipped)
-
-#for i in soup.find('div', class_='flipmove'):
-#    print(i)
-
-#def table_scrap(soup):
-#    pass
