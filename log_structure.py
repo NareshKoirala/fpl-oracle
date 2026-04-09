b69c928844a0d6011204f73239d2b676b53bc3c7
Naresh Koirala <koiralanaresh10@gmail.com>
2026-04-08 22:41:56 -0600

    Add Logger class for logging messages to a file
    - Implemented a Logger class in utils/log.py
    - Supports logging messages with different severity levels (INFO, ERROR)
    - Logs are saved in a dedicated "Logs" directory with timestamps

--- log_structure.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-folder_location = './logs'
-
-print(dir(datetime))
---
+++ utils/log.py
@@ -0,0 +1,24 @@
+import datetime
+from pathlib import Path
+
+class Logger:
+    def __init__(self, fileName):
+        # Modern path handling
+        log_dir = Path.cwd() / "Logs"
+        log_dir.mkdir(parents=True, exist_ok=True)
+        fileName = f"{fileName}.log"
+        self.log_path = log_dir / fileName
+
+    def log(self, level, message):
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"{timestamp} - {level}: {message}"
+        
+        with open(self.log_path, 'a') as f:
+            f.write(log_entry + '\n')
+
+    def info(self, message):
+        self.log('INFO', message)
+
+    def error(self, message):
+        self.log('ERROR', message)
+        
